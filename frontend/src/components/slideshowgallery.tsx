80a7009a9d61aa3844a6d6786d1ef256d18235cc
claudiusgalo <galoclaudius@icloud.com>
2023-04-02 03:05:02 -0500

    Add cors stuff

--- frontend/src/components/slideshowgallery.tsx
+++
@@ -1,8 +0,0 @@
-
-//The slideshow will have to take in props from the image being displayed & the counter
-export function Slideshow(){
-    return(
-        <div className="flex "></div>
-    );
-
-}
